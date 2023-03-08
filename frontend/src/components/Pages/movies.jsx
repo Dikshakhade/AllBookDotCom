c459078936c8195e1a9317c71cb57471e7b03bfe
AbhishekKhande <abhi23khande@gmail.com>
2023-03-08 13:07:43 +0530

    Movies done!

--- frontend/src/components/Pages/movies.jsx
+++ frontend/src/components/Pages/movies.jsx
@@ -2,9 +2,91 @@ import React from "react";
 import "./Movies.css";
 import ViewNoLog from "../headers/ViewNoLog";
 const Movies = () => {
+  const MovieData = [
+    {
+      Title: "The Lion King",
+      Year: "2019",
+      Rating: 8.5,
+      Runtime: "118 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Mowgli: Legend of the Jungle",
+      Year: "2018",
+      Rating: 8.5,
+      Runtime: "104 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMjMzODc2NzU5MV5BMl5BanBnXkFtZTgwNTMwMTE3NjM@._V1_SX300.jpg",
+    },
+    {
+      Title: "Doctor Strange",
+      Year: "2016",
+      Rating: 8.5,
+      Runtime: "115 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BNjgwNzAzNjk1Nl5BMl5BanBnXkFtZTgwMzQ2NjI1OTE@._V1_SX300.jpg",
+    },
+    {
+      Title: "John Wick",
+      Year: "2014",
+      Rating: 8.5,
+      Runtime: "101 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg",
+    },
+    {
+      Title: "The Notebook",
+      Year: "2004",
+      Rating: 8.5,
+      Runtime: "123 min",
+      Poster:
+        "https://m.media-amazon.com/images/M/MV5BMTU2NjA1ODgzMF5BMl5BanBnXkFtZTgwMTM2MTI4MjE@._V1_SX300.jpg",
+    },
+  ];
   return (
     <>
       <ViewNoLog />
+      {MovieData.map((movie, index) => {
+        return (
+          <div className="movie-div" key={index}>
+            <div className="movie-parent-div">
+              <div
+                className="movie"
+                id="movie-pic"
+                style={{ backgroundImage: `url("${movie.Poster}")` }}
+              >
+                {/* <img
+                  src=""
+                  alt=""
+                  style={{
+                    backgroundSize: "cover",
+                    backgroundRepeat: "no-repeat",
+                    // backgroundPosition: "center",
+                  }}
+                /> */}
+              </div>
+              <div className="movie" id="movie-description">
+                <div className="movie-des" id="movie-name">
+                  {movie.Title}
+                </div>
+                <div className="movie-des" id="movie-rating">
+                  {movie.Rating} /10
+                </div>
+                <div className="movie-des" id="movie-duration">
+                  Duration : {movie.Runtime}
+                </div>
+                <div className="movie-des" id="movie-year">
+                  Year : {movie.Year}
+                </div>
+              </div>
+              <div className="movie" id="movie-bookseat">
+                Book Seat
+              </div>
+            </div>
+          </div>
+        );
+      })}
     </>
   );
 };
