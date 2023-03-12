914eab0e13ac832d840dd6e6b84944583dc88601
Dikshakhade <dikshakhade305@gmail.com>
2023-03-12 18:41:56 +0530

    nothing to add

--- frontend/src/components/Pages/train.jsx
+++ frontend/src/components/Pages/train.jsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { useState, useEffect } from "react";
 import "./Train.css";
 import ViewNoLog from "../headers/ViewNoLog";
@@ -11,70 +10,13 @@ const Train = () => {
       console.log(res.data);
     });
   }, []);
-=======
-import React from "react";
-import "./Train.css";
-import ViewNoLog from "../headers/ViewNoLog";
-const Train = () => {
-  const TrainData = [
-    {
-      busId: 1,
-      name: "train1",
-      from: "Pune",
-      to: "Mumbai",
-      departureTime: "2.00pm",
-      totalTime: "4 hours",
-      price: "1000/-",
-    },
-    {
-      trainId: 2,
-      name: "train2",
-      from: "Pune",
-      to: "Banglore",
-      departureTime: "2.00pm",
-      totalTime: "4 hours",
-      price: "1000/-",
-    },
-    {
-      trainId: 3,
-      name: "train3",
-      from: "Pune",
-      to: "Ratnagiri",
-      departureTime: "2.00pm",
-      totalTime: "4 hours",
-      price: "1000/-",
-    },
-    {
-      trainId: 3,
-      name: "train3",
-      from: "Pune",
-      to: "Ratnagiri",
-      departureTime: "2.00pm",
-      totalTime: "4 hours",
-      price: "1000/-",
-    },
-    {
-      trainId: 3,
-      name: "train3",
-      from: "Pune",
-      to: "Ratnagiri",
-      departureTime: "2.00pm",
-      totalTime: "4 hours",
-      price: "1000/-",
-    },
-  ];
->>>>>>> 0b8f9f029cc671c26b2cff32f2a29ddf727cb117
 
   return (
     <>
       <ViewNoLog />
       {TrainData.map((Train) => {
         return (
-<<<<<<< HEAD
-          <div className="Train-div" key={Train.trainId}>
-=======
           <div className="Train-div" key={Train.TrainId}>
->>>>>>> 0b8f9f029cc671c26b2cff32f2a29ddf727cb117
             <div className="Train-element">
               <div className="Train-name" id="Train-description">
                 <div className="Train-p" id="Train-title">
